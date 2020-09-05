059f04bfcf335d7e63ab161838d8db7dd4fe2058
Jai <jaisinglanrw@gmail.com>
2020-09-05 23:22:55 +0530

    Level 5 auth

--- app.js
+++ app.js
@@ -7,12 +7,30 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 //const encrypt = require("mongoose-encryption");
 // const md5 = require("md5");
-const bcrypt = require("bcrypt");
+//const bcrypt = require("bcrypt");
+const passport = require("passport");
+const session = require("express-session");
+const passportLocalMongoose = require("passport-local-mongoose");
+
 app.set("view engine" , "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public/"));
+app.use(session({
+  secret: process.env.SECRET,
+  saveUninitialized: false,
+  resave: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+mongoose.set('useCreateIndex', true);
 
 mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+
+
 
 const userSchema = new mongoose.Schema(
   {
@@ -22,8 +40,14 @@ const userSchema = new mongoose.Schema(
 );
 //const secret = "Thisismysecret..";
 //userSchema.plugin(encrypt , {secret:process.env.SECRET , encryptedFields:['password']});
-const saltRound =10;
+userSchema.plugin(passportLocalMongoose);
+
+// use static serialize and deserialize of model for passport session support
+//const saltRound =10;
 const User = mongoose.model("User",userSchema);
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 app.get("/" , function(req , res){
   res.render("home");
 });
@@ -32,6 +56,20 @@ app.get("/login" , function(req , res){
 });
 app.get("/register" , function(req , res){
   res.render("register");
+});
+app.get("/secrets" , function(req , res){
+
+  if(req.isAuthenticated()){
+    res.render("secrets");
+  }
+  else{
+    res.redirect("/login");
+  }
+
+});
+app.get("/logout" , function(req , res){
+  req.logout();
+  res.redirect("/");
 });
 
 
@@ -52,34 +90,39 @@ app.get("/register" , function(req , res){
 //   })
 // });
 app.post("/register" , function(req , res){
-  bcrypt.hash(req.body.password , saltRound , function(err , hash){
-    const newUser = new User({
-      email:req.body.username,
-      password:hash
-    });
-    newUser.save(function(err){
-      if(err){
-        console.log(err);
-      }
-      else{
-        res.render("secrets");
-      }
-    });
+  User.register({username:req.body.username},req.body.password, function(err, user) {
+  if (err) {
+    console.log(err);
+    res.redirect("/register");
+  }
+  else{
+        passport.authenticate('local')(req , res , function(){
+          res.redirect("/secrets");
+        });
+        }
+    // Value 'result' is set to false. The user could not be authenticated since the user is not active
   });
-  });
+});
 
 app.post("/login" , function(req , res){
-  User.findOne({email : req.body.username} , function(err , foundUser){
-    //console.log(foundUser);
-    if(foundUser){
-      bcrypt.compare(req.body.password , foundUser.password , function(err ,result){
-
-        if(result){
-          res.render("secrets");
-        }
-
+  const user = new User({
+    username:req.body.username,
+    password:req.password
+  });
+  req.login(user , function(err){
+    if(err){
+      console.log(err);
+    }
+    else{
+      passport.authenticate('local')(req , res , function(){
+        res.redirect("/secrets");
       });
     }
+  });
+// app.post('/login',
+//   passport.authenticate('local', { successRedirect: '/secrets',
+//                                    failureRedirect: '/login' }));
+
        //console.log(req.body.password);
       // console.log(foundUser.password);
         // else{
@@ -88,7 +131,6 @@ app.post("/login" , function(req , res){
       // else{
       //   alert("Account not found Please register");
       // }
-  });
   });
 
 
