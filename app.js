70d4d17ddd107b9bd2a8c3502961c06a90c25b92
Jai <jaisinglanrw@gmail.com>
2020-09-05 11:20:27 +0530

    Level 4 Authentication

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 //const encrypt = require("mongoose-encryption");
-const md5 = require("md5");
+// const md5 = require("md5");
+const bcrypt = require("bcrypt");
 app.set("view engine" , "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public/"));
@@ -21,6 +22,7 @@ const userSchema = new mongoose.Schema(
 );
 //const secret = "Thisismysecret..";
 //userSchema.plugin(encrypt , {secret:process.env.SECRET , encryptedFields:['password']});
+const saltRound =10;
 const User = mongoose.model("User",userSchema);
 app.get("/" , function(req , res){
   res.render("home");
@@ -35,33 +37,54 @@ app.get("/register" , function(req , res){
 
 
 
+// app.post("/register" , function(req , res){
+//   const newUser = new User({
+//     email: req.body.username,
+//     password : md5(req.body.password)
+//   });
+//   newUser.save(function(err){
+//     if(err){
+//       console.log(err);
+//     }
+//     else{
+//       res.render("secrets");
+//     }
+//   })
+// });
 app.post("/register" , function(req , res){
-  const newUser = new User({
-    email: req.body.username,
-    password : md5(req.body.password)
+  bcrypt.hash(req.body.password , saltRound , function(err , hash){
+    const newUser = new User({
+      email:req.body.username,
+      password:hash
+    });
+    newUser.save(function(err){
+      if(err){
+        console.log(err);
+      }
+      else{
+        res.render("secrets");
+      }
+    });
   });
-  newUser.save(function(err){
-    if(err){
-      console.log(err);
-    }
-    else{
-      res.render("secrets");
-    }
-  })
-});
+  });
+
 app.post("/login" , function(req , res){
   User.findOne({email : req.body.username} , function(err , foundUser){
     //console.log(foundUser);
     if(foundUser){
+      bcrypt.compare(req.body.password , foundUser.password , function(err ,result){
+
+        if(result){
+          res.render("secrets");
+        }
+
+      });
+    }
        //console.log(req.body.password);
       // console.log(foundUser.password);
-      if(foundUser.password === md5(req.body.password)){
-        res.render("secrets");
-      }
         // else{
         //   alert("Incorrect username or password");
         // }
-      }
       // else{
       //   alert("Account not found Please register");
       // }
