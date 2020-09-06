77e78e2644ea0dc814f912bba098db80997c64ac
Jai <jaisinglanrw@gmail.com>
2020-09-06 11:16:00 +0530

    Level 6

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const mongoose = require("mongoose");
 const passport = require("passport");
 const session = require("express-session");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 app.set("view engine" , "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
@@ -34,20 +36,39 @@ mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true, use
 
 const userSchema = new mongoose.Schema(
   {
-    email : String ,
-    password:String
+    email : String,
+    password:String,
+    googleId: String,
+    secret:String
   }
 );
-//const secret = "Thisismysecret..";
-//userSchema.plugin(encrypt , {secret:process.env.SECRET , encryptedFields:['password']});
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 // use static serialize and deserialize of model for passport session support
-//const saltRound =10;
 const User = mongoose.model("User",userSchema);
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 app.get("/" , function(req , res){
   res.render("home");
 });
@@ -59,36 +80,38 @@ app.get("/register" , function(req , res){
 });
 app.get("/secrets" , function(req , res){
 
-  if(req.isAuthenticated()){
-    res.render("secrets");
-  }
-  else{
-    res.redirect("/login");
-  }
+  // if(req.isAuthenticated()){
+  User.find({secret:{$ne : null}} , function(err , foundUsers){
+
+    res.render("secrets" ,{ourSecrets:foundUsers});
+  });
+  // }
+  // else{
+  //   res.redirect("/login");
+  // }
 
 });
 app.get("/logout" , function(req , res){
   req.logout();
   res.redirect("/");
 });
+app.get("/submit" , function(req , res){
+  if(req.isAuthenticated()){
+    res.render("submit");
+  }
+  else{
+    res.redirect("/login");
+  }
+});
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ["profile"] }));
 
-
-
-
-// app.post("/register" , function(req , res){
-//   const newUser = new User({
-//     email: req.body.username,
-//     password : md5(req.body.password)
-//   });
-//   newUser.save(function(err){
-//     if(err){
-//       console.log(err);
-//     }
-//     else{
-//       res.render("secrets");
-//     }
-//   })
-// });
+app.get('/auth/google/secrets',
+  passport.authenticate('google', { failureRedirect: "/login" }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/secrets");
+  });
 app.post("/register" , function(req , res){
   User.register({username:req.body.username},req.body.password, function(err, user) {
   if (err) {
@@ -112,6 +135,7 @@ app.post("/login" , function(req , res){
   req.login(user , function(err){
     if(err){
       console.log(err);
+      res.redirect("/login");
     }
     else{
       passport.authenticate('local')(req , res , function(){
@@ -133,7 +157,20 @@ app.post("/login" , function(req , res){
       // }
   });
 
-
+app.post("/submit" , function(req , res){
+  if(req.isAuthenticated()){
+    const submittedSecret = req.body.secret;
+      User.findById(req.user.id ,function(err , foundUser){
+        foundUser.secret=submittedSecret;
+        foundUser.save(function(){
+              res.redirect("/secrets");
+        });
+  });
+    }
+    else{
+      res.redirect("/login");
+    }
+});
 
 app.listen(3000 , function(){
   console.log("Server started on port 3000");
